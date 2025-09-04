e29f1a67631d20b88364f5809f975abc431f871a
jmbravenec <jmbravenec@gmail.com>
2025-09-04 15:32:22 +0000

    fixed runtime errors

--- buggy.cpp
+++ buggy.cpp
@@ -14,41 +14,39 @@ private:
 
     int vertices;
     Point** points;
-    double* total_area;
 
 public:
 
     Shape (int _vertices) {
         vertices = _vertices;
-        points = new Point*[vertices+1];
+        points = new Point*[vertices];
     }
 
    ~Shape () {
 
-	   for(int i = 0; i <= vertices; i++) {
+	   for(int i = 0; i < vertices; i++) {
 		   delete points[i];
 	   }
 
-	   delete total_area;
 	   delete[] points;
     }
 
     void addPoint(Point* pts) {
-        for (int i = 0; i <= vertices; i++) {
-		points[i] = new Point();
-            	memcpy(points[i], &pts[i%vertices], sizeof(Point));
+        for (int i = 0; i < vertices; i++) {
+		    points[i] = new Point();
+            memcpy(points[i], &pts[i%vertices], sizeof(Point));
+            //*(points[i]) = pts[i];
         }
     }
 
-    double* area () {
+    double area () {
         int temp = 0;
         for (int i = 0; i < vertices; i++) {
-            	int lhs = points[i]->x * points[i+1]->y;
-            	int rhs = (*points[i+1]).x * (*points[i]).y;
+            	int lhs = points[i]->x * points[(i+1) % vertices]->y;
+            	int rhs = (*points[(i+1) % vertices]).x * (*points[i]).y;
             	temp += (lhs - rhs);
         }
-        double* total_area = new double(abs(temp)/2.0);
-        return total_area;
+        return abs(temp)/2.0;
     }
 };
 
@@ -73,6 +71,9 @@ int main () {
     quad->addPoint(quadPts);
 
     
-    std::cout << "tri area: " << *(tri->area()) << std::endl;
-    std::cout << "quad area: " << *(quad->area()) << std::endl;
+    std::cout << "tri area: " << tri->area() << std::endl;
+    std::cout << "quad area: " << quad->area() << std::endl;
+
+    delete tri;
+    delete quad;
 }
